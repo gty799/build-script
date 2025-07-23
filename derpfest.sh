51eee3b18603f8a4bc68f91336bd0d4f72483bea
gty799 <59225496+gty799@users.noreply.github.com>
2025-07-23 12:34:32 +0300

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -14,6 +14,7 @@ echo "========================="
 
 # Build Sync
 /opt/crave/resync.sh
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 echo "=============================="
 echo "Master the Sync is now Success"
 echo "=============================="
