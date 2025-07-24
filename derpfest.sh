5f2b72a87c2a09d4b6eedd492df4d3e211200d22
gty799 <59225496+gty799@users.noreply.github.com>
2025-07-24 16:42:43 +0300

    Update derpfest.sh

--- derpfest.sh
+++ derpfest.sh
@@ -22,7 +22,8 @@ echo "=============================="
 # Sync Trees
 git clone https://github.com/mitmac29/android_device_xiaomi_lavender16.git -b clover16 device/xiaomi/lavender
 git clone https://github.com/pix106/android_vendor_xiaomi_lavender.git -b 16 vendor/xiaomi/lavender
-git clone https://github.com/pix106/android_kernel_xiaomi_southwest-4.19.git kernel/xiaomi/lavender
+git clone https://github.com/mitmac29/android_kernel_xiaomi_southwest-4.19.git -b main-dynamic kernel/xiaomi/sdm660
+git clone https://github.com/mitmac29/android_kernel_xiaomi_southwest-4.19.git -b main-dynamic kernel/xiaomi/lavender
 git clone https://github.com/mitmac29/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
 git clone https://github.com/mitmac29/android_device_xiaomi_sdm660-common.git -b clover16 device/xiaomi/sdm660-common
 git clone https://github.com/pix106/android_vendor_xiaomi_sdm660-common.git -b 16 vendor/xiaomi/sdm660-common
